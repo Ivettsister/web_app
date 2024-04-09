d60a9f8d39cf769477227907427c63af087c9621
Violetta <soloveva.vd@phystech.edu>
2024-04-10 00:45:01 +0300

    Set settings and first web example

--- external_app/app/settings.py
+++ external_app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9)qsr@q^c)i*%orx64huq%wm1=now)#@0m)d6v@ipnm5_p--(4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'main',
 ]
 
 MIDDLEWARE = [
