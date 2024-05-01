03e679ef1baa55b96ea23b622ddb4cdc5aa396b4
Violetta <soloveva.vd@phystech.edu>
2024-05-01 14:57:53 +0300

    done common project static, base.html

--- external_app/main/views.py
+++ external_app/main/views.py
@@ -4,11 +4,8 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Home",
-        "content": "Главная страница магазина Home",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": False,
+        "title": "Home - главная",
+        "content": "Сайт репетитора Соловьевой Виолетты",
     }
 
     return render(request, "main/index.html", context)
