904c1de075f9849afe32bb76792668a90085fe28
Violetta <soloveva.vd@phystech.edu>
2024-05-09 22:49:13 +0300

    Search is ready!

--- external_app/goods/views.py
+++ external_app/goods/views.py
@@ -2,19 +2,25 @@ from unicodedata import category
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
+from goods.utils import q_search
 from goods.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
     
     if category_slug == 'all':
         goods = Products.objects.all()
+    
+    elif query:
+        goods = q_search(query)
+    
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = Products.objects.filter(category__slug=category_slug)
     
     if on_sale:
         goods = goods.filter(discount__gt=0)
@@ -26,7 +32,7 @@ def catalog(request, category_slug):
     current_page = paginator.page(int(page))
     
     context = {
-        "title": "Каталог программ:",
+        "title": "Каталог программ",
         "goods": current_page,
         "slug_url": category_slug,
     }
